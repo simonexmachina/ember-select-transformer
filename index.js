d733d57c461d7e8f41c831ad44befd866cd30d48
Simon Wade <simon.wade@gmail.com>
2014-10-27 17:00:35 +1100

    Update select-transformer version and use new ember-addon style

--- index.js
+++ index.js
@@ -1,34 +1,24 @@
 /* jshint node: true */
 var path = require('path');
 
-var EmberAddon = function() {};
+module.exports = {
+  name: 'ember-select-transformer',
+  treeFor: function(type) {
+    if (type === 'vendor') {
+      return this.treeGenerator(path.join(__dirname, 'node_modules'));
+    }
+    else {
+      return this._treeFor(type);
+    }
+  },
+  included: function(app) {
+    this._super.included(app);
 
-EmberAddon.prototype.treeFor = function treeFor(type) {
-  if (type == 'app') {
-    var tree = path.join('node_modules', 'ember-select-transformer', 'ember-addon');
-    return unwatchedTree(tree);
-	}
-  else if (type == 'vendor') {
-    var tree = path.join('node_modules', 'ember-select-transformer', 'node_modules');
-    return unwatchedTree(tree);
+    this.app.import('vendor/select-transformer/dist/named-amd/main.js', {
+      exports: {
+        'select-transformer': ['default']
+      }
+    });
+    this.app.import('vendor/select-transformer/assets/default.css');
   }
 };
-
-EmberAddon.prototype.included = function included(app) {
-  this.app = app;
-  this.app.import('vendor/select-transformer/dist/named-amd/main.js', {
-    exports: {
-      'select-transformer': ['default']
-    }
-  });
-  this.app.import('vendor/select-transformer/assets/default.css');
-};
-
-function unwatchedTree(dir) {
-  return {
-    read:    function() { return dir; },
-    cleanup: function() { }
-  };
-}
-
-module.exports = EmberAddon;
